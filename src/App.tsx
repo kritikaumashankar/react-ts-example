77d5f67dba76e89c307128e5b4f5ae2fffb02435
Shiv Kumar Subramaniam <shivsubramaniam@shivs-mac-mini.lan>
2023-03-31 10:34:13 -0600

    first react ts example

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+import Counter from "./components/Counter";
+import List from "./components/List";
 
-function App() {
+
+function App(){
+  const [count, setCount] = useState(1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Heading title={"Kritika"}/>
+      <Section title={"Diff Title"}>This is my section</Section>
+      <Counter setCount={setCount}> Count is {count}</Counter>
+      <List items={["coffee","tacos","code"]} render={(item: string) => <span>{item}</span>} />
+    </>
+    )
 }
 
 export default App;
